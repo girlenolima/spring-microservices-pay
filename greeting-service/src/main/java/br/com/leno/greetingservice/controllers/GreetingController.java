ea9329992767ca02d73e1fbc9763e04f0c69513c
Girleno Lima <girlenosilveira@gmail.com>
2024-03-24 09:50:46 -0300

    create config server local

---
+++ greeting-service/src/main/java/br/com/leno/greetingservice/controllers/GreetingController.java
@@ -0,0 +1,35 @@
+package br.com.leno.greetingservice.controllers;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import br.com.leno.greetingservice.configuration.GreetingConfig;
+import br.com.leno.greetingservice.model.Greeting;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+	private static final String template = "%s, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@Autowired
+	private GreetingConfig config;
+
+	@RequestMapping("/greeting")
+	public Greeting greeting(
+			@RequestParam(value="name",
+			defaultValue = "") String name) {
+
+		if(name.isEmpty()){
+			name = config.getDefaultValue();
+		}
+
+		return new Greeting(
+					counter.incrementAndGet(),
+					String.format(template, config.getGreeting(),name)
+				);
+	}
+}
